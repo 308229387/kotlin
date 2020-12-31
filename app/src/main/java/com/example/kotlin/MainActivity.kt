b43f698f622eed3a9ceb7cf441a57474262c4431
admin <admin@admindeMacBook-Pro.local>
2020-12-31 18:34:00 +0800

    first commit

---
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView(){
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recylerView.layoutManager = layoutManager
+
+        val adapter = HobbiesAdapter(this, HobbiesData.hobbies)
+        recylerView.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/com/example/kotlin/Model.kt
@@ -0,0 +1,32 @@
+package com.example.kotlin
+
+data class Hobby(var title: String)
+
+object HobbiesData {
+
+    val hobbies = listOf(
+        Hobby("Gaming"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Playing"),
+        Hobby("Gaming"),
+        Hobby("Swimming"),
+        Hobby("Football"),
+        Hobby("Playing"),
+        Hobby("Cricket"),
+        Hobby("Swimming"),
+        Hobby("Chess"),
+        Hobby("Movies"),
+        Hobby("Football"),
+        Hobby("Playing"),
+        Hobby("Programming"),
+        Hobby("Swimming"),
+        Hobby("Chess"),
+        Hobby("Movies"),
+        Hobby("Football"),
+        Hobby("Playing"),
+        Hobby("Cricket"),
+        Hobby("Swimming"),
+        Hobby("Chess"),
+        Hobby("Movies"))
+}
