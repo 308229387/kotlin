83eaca87458758a35fc8cce50e343a504b6137c0
songyongmeng <songyongmeng@yidian-inc.com>
2021-02-02 20:00:54 +0800

    添加测量键盘高度

--- app/src/main/java/com/example/kotlin/activity/KeyboardActivity.kt
+++ app/src/main/java/com/example/kotlin/activity/KeyboardActivity.kt
@@ -1,12 +1,24 @@
 package com.example.kotlin.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin.R
+import com.example.kotlin.utils.HeightProvider
+import kotlinx.android.synthetic.main.activity_nt_nothing.*
 
 class KeyboardActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "KeyboardActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nt_nothing)
+
+        HeightProvider(this).init().setHeightListener { height ->
+            Log.v(TAG, "获取到键盘高度$height")
+            etBottom.translationY = -height.toFloat()
+        }
     }
 }
