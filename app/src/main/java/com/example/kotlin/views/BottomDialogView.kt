1886bec15f7e9ef5a281fa14be73d4ec1eeb1242
308229387 <308229387@qq.com>
2021-03-06 03:56:17 +0800

    不顶起键盘

--- app/src/main/java/com/example/kotlin/views/BottomDialogView.kt
+++ app/src/main/java/com/example/kotlin/views/BottomDialogView.kt
@@ -1,14 +1,16 @@
 package com.example.kotlin.views
 
 import android.content.Context
+import android.view.Gravity
 import android.view.View
-import android.widget.ImageView
-import android.widget.RelativeLayout
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.FragmentActivity
 import com.example.kotlin.R
-import com.example.kotlin.activity.BottomDialogActivity
 import com.example.kotlin.fragment.AddCommentFragment
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.dialog_bottomsheet.view.*
 
 /**
  * FileName: BottomDialogView
@@ -21,9 +23,16 @@ class BottomDialogView : BottomSheetDialog {
     constructor(context: Context) : this(context, R.style.dialog)
 
     constructor(context: Context, theme: Int) : super(context, theme) {
+
+        window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+
+
         val view = View.inflate(context, R.layout.dialog_bottomsheet, null)
-        val iv_dialog_close = view.findViewById<View>(R.id.dialog_bottomsheet_iv_close) as ImageView
-        val rl_comment = view.findViewById<View>(R.id.rl_comment) as RelativeLayout
+        val iv_dialog_close = view.dialog_bottomsheet_iv_close
+        val rl_comment = view.rl_comment
+        val mWebView = view.dialog_web_view
+        mWebView.loadUrl("www.baidu.com");//加载url
+
         iv_dialog_close.setOnClickListener { dismiss() }
         rl_comment.setOnClickListener {
             if (context is FragmentActivity)
