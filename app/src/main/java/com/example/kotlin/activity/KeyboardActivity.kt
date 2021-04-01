f908f10ae495ff3e1dd46ea06ab459e4e9488f6f
songyongmeng <songyongmeng@yidian-inc.com>
2021-04-01 10:51:53 +0800

    add 添加计算导航栏高度

--- app/src/main/java/com/example/kotlin/activity/KeyboardActivity.kt
+++ app/src/main/java/com/example/kotlin/activity/KeyboardActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin.activity
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin.R
@@ -17,8 +19,43 @@ class KeyboardActivity : AppCompatActivity() {
         setContentView(R.layout.activity_nt_nothing)
 
         SoleHeightProvider(this).init().setHeightListener { height ->
-            height_result.text = "键盘高度为$height"
+            height_result.text = "键盘高度为$height     导航栏高度为${getNavigationHeight(this)} "
             Log.d(TAG, "键盘高度为$height")
         }
     }
+
+
+    private fun getNavigationHeight(context: Context): Int {
+        var windowheight: Int = windowManager.defaultDisplay.height
+        Log.d("song_test", "无导航栏状态栏时窗口高度 = $windowheight")
+        var fullheigh = 0;   //窗口总高度
+        var display = windowManager.defaultDisplay
+        var dm = DisplayMetrics()
+        try {
+            var klass = Class.forName("android.view.Display")
+
+            var method = klass.getMethod("getRealMetrics", DisplayMetrics::class.java)
+            method.invoke(display, dm)
+            fullheigh = dm.heightPixels
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        Log.d("song_test", "有导航栏状态栏时窗口高度  fullheigh = $fullheigh")
+        if (windowheight == fullheigh) return 0;   //无虚拟导航栏存在
+        Log.d("song_test", "栏状态栏高度   =   ${getStatusBarHeight(context)}");
+
+        return fullheigh - windowheight - getStatusBarHeight(context);  //导航栏高度
+    }
+
+    fun getStatusBarHeight(context: Context): Int {
+        var result = 0
+        val resourceId = context.resources
+            .getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = context.resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
 }
