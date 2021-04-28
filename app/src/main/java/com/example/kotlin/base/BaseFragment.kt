903e0c162a672074d34dc8b67c4a56fcc52f59d0
songyongmeng <songyongmeng@yidian-inc.com>
2021-04-28 22:19:49 +0800

    添加fragment  viewbind

---
+++ app/src/main/java/com/example/kotlin/base/BaseFragment.kt
@@ -0,0 +1,33 @@
+package com.example.kotlin.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+/**
+ * Author: sym
+ * Date: 2021/4/28 8:46 PM
+ * Describe:
+ */
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    lateinit var viewBind:VB
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewBind = createViewBinding(inflater,container)
+        return viewBind.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        init(savedInstanceState)
+    }
+
+    abstract fun createViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    protected open fun init(savedInstanceState: Bundle?) {}
+
+}
--- app/src/main/java/com/example/kotlin/fragment/FragmentTwo.kt
+++ app/src/main/java/com/example/kotlin/fragment/FragmentTwo.kt
@@ -1,27 +1,30 @@
 package com.example.kotlin.fragment
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.kotlin.R
-import kotlinx.android.synthetic.main.fragment_one_layout.view.*
+import com.example.kotlin.base.BaseFragment
+import com.example.kotlin.databinding.FragmentOneLayoutBinding
 
 /**
  * Author: sym
  * Date: 2021/4/28 5:49 PM
  * Describe:
  */
-class FragmentTwo : Fragment() {
-    @SuppressLint("ResourceAsColor")
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_one_layout, null)
-        view.setBackgroundColor(Color.parseColor("#000000"))
-        view.fragment_text.text = "Fragment Two"
-        view.fragment_text.setTextColor(Color.parseColor("#ffffff"))
-        return view
+class FragmentTwo : BaseFragment<FragmentOneLayoutBinding>() {
+
+    override fun init(savedInstanceState: Bundle?) {
+        super.init(savedInstanceState)
+        viewBind.fragmentBackLayout.setBackgroundColor(Color.parseColor("#000000"))
+        viewBind.fragmentText.text = "Fragment Two"
+        viewBind.fragmentText.setTextColor(Color.parseColor("#ffffff"))
     }
+
+
+    override fun createViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentOneLayoutBinding {
+        return FragmentOneLayoutBinding.inflate(inflater, container, false)
+    }
+
 }
