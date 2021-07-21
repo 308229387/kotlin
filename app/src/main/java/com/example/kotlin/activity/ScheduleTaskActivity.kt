da0ee66952dc1b45246633e11331490ec6c32813
308229387 <308229387@qq.com>
2021-07-21 17:13:24 +0800

    +

--- app/src/main/java/com/example/kotlin/activity/ScheduleTaskActivity.kt
+++ app/src/main/java/com/example/kotlin/activity/ScheduleTaskActivity.kt
@@ -1,11 +1,10 @@
 package com.example.kotlin.activity
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin.R
+import com.example.kotlin.utils.ScheduleSearchTextUtil
 import kotlinx.android.synthetic.main.activity_schedule_task.*
-import java.util.*
 
 /**
  * Author: sym
@@ -13,25 +12,10 @@ import java.util.*
  * Describe:定时任务实现
  */
 class ScheduleTaskActivity : AppCompatActivity() {
-    private var timer: Timer = Timer()
-    private var i = 0
-
-    private val handler: Handler = Handler {
-        schedule_text.text = it.arg1.toString()
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule_task)
-        timer.schedule(object : TimerTask() {
-            override fun run() {
-                var message = handler.obtainMessage()
-                message.arg1 = i
-                handler.sendMessage(message)
-                i++
-            }
-
-        }, 0, 1000)
+        ScheduleSearchTextUtil.get().setTextToSearch(schedule_text)
     }
 }
---
+++ app/src/main/java/com/example/kotlin/utils/ScheduleSearchTextUtil.kt
@@ -0,0 +1,64 @@
+package com.example.kotlin.utils
+
+import android.os.Handler
+import android.os.Message
+import android.widget.TextView
+import java.util.*
+
+/**
+ * Author: sym
+ * Date: 2021/7/21 4:58 PM
+ * Describe:支持多个地方共同配置文字
+ */
+class ScheduleSearchTextUtil {
+    private var i = 0
+    private var textView1: TextView? = null
+    private var textView2: TextView? = null
+    private val handler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            if (msg.arg1 == 200) {
+                val result: Int = i % 5
+                textView1?.text = "默认$result"
+                textView2?.text = "默认$result"
+            }
+        }
+    }
+
+    //单例
+    companion object {
+        private var instance: ScheduleSearchTextUtil? = null
+            get() {
+                if (field == null) {
+                    field = ScheduleSearchTextUtil()
+                }
+                return field
+            }
+
+        fun get(): ScheduleSearchTextUtil {
+            return instance!!
+        }
+    }
+
+
+    constructor() {
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val message: Message = handler.obtainMessage()
+                message.arg1 = 200
+                handler.sendMessage(message)
+                i++
+            }
+        }, 0, 1000)
+    }
+
+    fun setTextToSearch(textView: TextView?) {
+        textView1 = textView
+    }
+
+    fun setTextToSearch1(textView: TextView?) {
+        textView2 = textView
+    }
+
+
+}
