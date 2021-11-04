7a26dbd7e377080bc999aaa7d732ba19af65b362
308229387 <308229387@qq.com>
2021-11-04 12:01:45 +0800

    完善next

--- app/src/main/java/com/example/kotlin/adapter/QAAdapter.kt
+++ app/src/main/java/com/example/kotlin/adapter/QAAdapter.kt
@@ -1,13 +1,11 @@
 package com.example.kotlin.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.R
-import com.example.kotlin.activity.QADetailActivity
 import com.example.kotlin.data.HawkConfig
 import com.example.kotlin.data.QAItemData
 import com.example.kotlin.utils.QAAdapterJumpUtil
@@ -45,25 +43,43 @@ class QAAdapter(private val context: Context, private val dataList: ArrayList<QA
 
                 val simpleDateFormat = SimpleDateFormat("yyyy年MM月dd日")
                 dataList[position].lastTime = simpleDateFormat.format(Date(System.currentTimeMillis()))
-                dataList[position].nextTime = ToolsUtil.beforeAfterDate(1).toString()
+                dataList[position].tag++ //标记为处理状态
 
-
-                if(!specialList.contains(dataList[position])){
-                    specialList.add(dataList[position])
-                    Hawk.put(HawkConfig.SpecialQA, specialList)
-                    Toast.makeText(context, "已保存", Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(context, "已在关注列表", Toast.LENGTH_SHORT).show()
+                when {
+                    dataList[position].tag == 1 -> {
+                        dataList[position].nextTime = ToolsUtil.beforeAfterDate(1).toString()
+                    }
+                    dataList[position].tag == 2 -> {
+                        dataList[position].nextTime = ToolsUtil.beforeAfterDate(2).toString()
+                    }
+                    dataList[position].tag == 3 -> {
+                        dataList[position].nextTime = ToolsUtil.beforeAfterDate(4).toString()
+                    }
+                    dataList[position].tag == 4 -> {
+                        dataList[position].nextTime = ToolsUtil.beforeAfterDate(7).toString()
+                    }
+                    dataList[position].tag > 4 -> {
+                        dataList[position].nextTime = ToolsUtil.beforeAfterDate((dataList[position].tag-4)*15).toString()
+                    }
                 }
 
+                if (!specialList.contains(dataList[position])) {
+                    specialList.add(dataList[position])
+                    Toast.makeText(context, "已保存", Toast.LENGTH_SHORT).show()
+                } else {
+                    specialList[specialList.indexOf(dataList[position])] = dataList[position]
+                    Toast.makeText(context, "已更新", Toast.LENGTH_SHORT).show()
+                }
+                Hawk.put(HawkConfig.SpecialQA, specialList)
 
-                dataList[position].tag = 1 //标记为处理状态
+
+
                 notifyDataSetChanged()
                 Hawk.put(HawkConfig.QA, dataList)
             }
 
             override fun jump() {
-                QAAdapterJumpUtil.setIntent(context,dataList[position].title)
+                QAAdapterJumpUtil.setIntent(context, dataList[position].title)
             }
 
             override fun longClick() {
